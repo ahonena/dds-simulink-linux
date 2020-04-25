b47d8e3e72b6d434c8919f7b91efb0b52a301fef
ahonena <andrei.ahonen@tuni.fi>
2020-04-25 17:27:47 +0300

    More small edits

--- add_legacy_macros.sh
+++ add_legacy_macros.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MY_FILE=m12_localization_ert_linux/ert_main.c
+export MY_FILE=my_model_ert_linux/ert_main.c
 sed -i '1s/^/#ifndef rtmGetStopRequested \n/' $MY_FILE;
 sed -i '2s/^/#define rtmGetStopRequested(rtm)      ((void*) 0) \n/' $MY_FILE;
 sed -i '3s/^/#endif \n/' $MY_FILE;
